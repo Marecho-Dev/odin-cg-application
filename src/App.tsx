723da6580b8edadaaf9a4f9132de8b4be56bbc84
Marek Staszkiewicz <Marekstaszkiewicz@live.com>
2023-08-08 15:14:57 -0400

    style up

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
@@ -8,7 +6,7 @@ function App() {
     <>
       <div className="container">
         <div className="form">
-          <input className="name"></input>
+          <input></input>
           <input></input>
           <input></input>
         </div>
